45252dc593e0c940781c201f7dc27dd7c8748da4
Thiago Pires Fernandes <thiagopiresfernandes7@gmail.com>
2026-07-28 10:39:52 -0300

    Add secure freemium entitlement foundation

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME="cockpit-financeiro-v12";
+const APP_SHELL=["./","./index.html","./freemium.js","./manifest.json","./privacy-policy.html","./terms.html","./support.html","./delete-account.html","./icon-192.svg","./icon-512.svg"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET"||new URL(event.request.url).origin!==self.location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response&&response.ok){const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy))}
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match("./index.html"))));
+});
